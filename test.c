ffa037c7d47c2b5ad48ea6aba0d54d22ea7b1473
gugu256 <gugu256@mail.com>
2025-10-14 17:05:15 +0200

    init

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "color.h"
+
+int main() {
+    printf("Hello world\n");
+    printf("%s%s%s%s%sYou can also get crazy with it%s%s%s!!%s\n", BACK_GREEN, FORE_RED, STYLE_UNDERLINE, STYLE_BOLD, STYLE_BLINK, RESET_ALL, BACK_BLUE, FORE_YELLOW, RESET_ALL);
+    return 0;
+}
